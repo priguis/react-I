930ebc1186e3c62735ea0b6b352d497091f7af38
Priscilla Alves <Priihalves_@hotmail.com>
2019-11-12 14:54:10 -0300

    primeiro commit

---
+++ aula2/src/App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import './App.css';   
+
+class Contador extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contador: 0
+    };
+  }
+  
+
+  adicionarUm = () => {
+    this.setState((prevState) => {
+      return {
+        contador: prevState.contador + 1
+      }
+    })
+  }
+
+  menosUm = () => {
+    this.setState((prevState)=> {
+      return {
+        contador: prevState.contador -1
+      }
+    })
+  }
+apagaTudo = () => {
+  this.setState(() =>{
+    return {
+      contador: 0 
+    }
+  })
+}
+
+  render() {
+    return (
+      <div>
+        <p>{this.state.contador}</p>
+        <button className="btnMaisUm" onClick={this.adicionarUm}>+1</button>
+        <button className="btnMenosUm"onClick={this.menosUm}>-1</button>
+        <button className="btnResetar" onClick={this.apagaTudo}>resetar</button>
+      </div>
+    );
+  }
+}
+
+function App(props) {
+  return (
+    <div className="App">
+      <h2>Contador</h2>
+      <Contador />
+    </div>
+  );
+}
+export default App;
